432b1471ca99c8b53a8f146da59b4dcafd1bf8bf
Jeganathan Swaminathan <mail2jegan@gmail.com>
2025-03-11 17:56:40 +0530

    Added Day7 lab exercises

---
+++ Day7/declarative-manifest-scripts/after-refactoring/deploy.sh
@@ -0,0 +1,14 @@
+echo "\nDeploying mariadb ..."
+oc apply -f wordpress-configmap.yml
+oc apply -f wordpress-secrets.yml
+oc apply -f mariadb-pv.yml
+oc apply -f mariadb-pvc.yml
+oc apply -f mariadb-deploy.yml
+oc apply -f mariadb-service.yml
+
+echo "\nDeploying wordpress ..."
+oc apply -f wordpress-pv.yml
+oc apply -f wordpress-pvc.yml
+oc apply -f wordpress-deploy.yml
+oc apply -f wordpress-service.yml
+oc apply -f wordpress-route.yml
---
+++ Day7/declarative-manifest-scripts/after-refactoring/undeploy.sh
@@ -0,0 +1,14 @@
+echo "\nUndeploying wordpress ..."
+oc delete -f wordpress-route.yml
+oc delete -f wordpress-service.yml
+oc delete -f wordpress-deploy.yml
+oc delete -f wordpress-pvc.yml
+oc delete -f wordpress-pv.yml
+
+echo "\nUndeploying mariadb ..."
+oc delete -f mariadb-service.yml
+oc delete -f mariadb-deploy.yml
+oc delete -f mariadb-pvc.yml
+oc delete -f mariadb-pv.yml
+oc delete -f wordpress-configmap.yml
+oc delete -f wordpress-secrets.yml
